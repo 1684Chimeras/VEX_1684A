c12dabcfb5b264b78de6fe824cc8b9d8493931c8
Jake Niman <illidans911@gmail.com>
2016-04-28 20:37:51 -0400

    Initial hook encoder stuff

--- 2016CompetitionRobot/src/modules/climber.py
+++ 2016CompetitionRobot/src/modules/climber.py
@@ -10,6 +10,10 @@ class Climber(object):
     classdocs
     '''
 
+    min = 0.0
+    max = 4096.0
+    angle_min = 0
+    throw = 180.0
 
     def __init__(self, pulley, tape):
         '''
@@ -18,6 +22,15 @@ class Climber(object):
         self.pulley = wpilib.CANTalon(pulley)
         self.tape = wpilib.VictorSP(tape)
         
+        self.encoderMotor = self.pulley
+        self.encoderMotor.setFeedbackDevice(7)
+        
+    def getHookPosition(self):
+        return self.angle_min + (1 - (self.max - self.getHookEncoder()) / (self.max - self.min)) * self.throw
+    
+    def getHookEncoder(self):
+        return self.encoderMotor.getPulseWidthPosition()    
+    
     def setPulley(self,value):
         self.pulley.set(value)
         
